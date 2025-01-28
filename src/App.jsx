9748f4fd744ab8b2c9fe70d0c6c434e03957262b
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-01-28 18:32:34 +0600

    file setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from './assets/component/Container'
+
+const App = () => {
+  return (
+    <div className='bg-yellow_Color'>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yellow_Color : "var(--yellow_color)",
+        text_black_color : "var(--text_black_color)",
+        bg_gray_one : "var(--bg_gray_one)",
+        bg_gray_two : "var(--bg_gray_two)",
+        bg_white_one : "var(--bg_white_one)",
+        bg_white : "var(--bg_white)"
+      }
+    },
+  },
+  plugins: [],
+}
