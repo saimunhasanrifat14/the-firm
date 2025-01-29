6b72c5747431e65976ab09b7e404120026932215
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-01-29 23:12:19 +0600

    created perter part

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./component/Header";
 import Banner from "./component/banner";
+import Partner from "./component/Partner";
 const App = () => {
   return (
     <>
       <Header></Header>
       <Banner></Banner>
+      <Partner></Partner>
     </>
   );
 };
---
+++ src/component/Partner.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Container from './CommonComponent/Container'
+import PerterLogo1 from '../assets/PerterLogo1.png'
+import PerterLogo2 from '../assets/PerterLogo2.png'
+import PerterLogo3 from '../assets/PerterLogo3.png'
+import PerterLogo4 from '../assets/PerterLogo4.png'
+
+const Partner = () => {
+  return (
+    <Container>
+        <div>
+            <h3 className='topTitle text-[40px] font-black text-text_black_color text-center font-nunito mb-[50px] mt-[100px]'>Our Partner</h3>
+            <div className='flex justify-between mb-[100px]'>
+                <div className="w-[240px] h-[120px] rounded-lg bg-bg_gray_two flex justify-center items-center">
+                    <picture>
+                        <img src={PerterLogo1} alt={PerterLogo1} />
+                    </picture>
+                </div>
+                <div className="w-[240px] h-[120px] rounded-lg bg-bg_gray_two flex justify-center items-center">
+                    <picture>
+                        <img src={PerterLogo2} alt={PerterLogo2} />
+                    </picture>
+                </div>
+                <div className="w-[240px] h-[120px] rounded-lg bg-bg_gray_two flex justify-center items-center">
+                    <picture>
+                        <img src={PerterLogo3} alt={PerterLogo3} />
+                    </picture>
+                </div>
+                <div className="w-[240px] h-[120px] rounded-lg bg-bg_gray_two flex justify-center items-center">
+                    <picture>
+                        <img src={PerterLogo4} alt={PerterLogo4} />
+                    </picture>
+                </div>
+            </div>
+        </div>
+    </Container>
+  )
+}
+
+export default Partner
